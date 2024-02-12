983492c3f40b09be5641e2484db8884aac9226a2
Requlix <rayrayzkc@gmail.com>
2024-02-12 14:35:03 -0600

    made map load from file work

--- Scramble/Level.cpp
+++ Scramble/Level.cpp
@@ -1,1 +1,45 @@
 #include "Level.h"
+
+Level::Level()
+{
+
+}
+
+Level::~Level()
+{
+
+}
+
+//takes information from file to make level 
+void Level::loadFromFile(std::ifstream file, std::string fileName)
+{
+    std::string temp;
+
+    while (file.is_open())
+    {
+        file >> temp;
+        file.close();
+    }
+
+    while (temp.size() > 0)
+    {
+        for (int i = 0; i < 25; i++)
+        {
+            levelArrVec[i % 25].push_back(temp.at(0));
+            temp.erase(0, 1);
+        }
+    }
+}
+
+//loads each level into sfml
+void Level::loadLevel()
+{
+
+}
+
+//checks for position of pieces in each type of 
+//piece and idetifies if it has struck something
+bool Level::checkCollision(Player user)
+{
+
+}
--- Scramble/Level.h
+++ Scramble/Level.h
@@ -1,5 +1,18 @@
 #pragma once
+#include <vector>
+#include <string>
+#include <fstream>
+#include "Player.h"
+
 class Level
 {
+public:
+	Level();
+	~Level();
+	void loadFromFile(std::ifstream, std::string);
+	void loadLevel();
+	bool checkCollision(Player);
+private:
+	std::vector<char> levelArrVec[25]
 };
 
